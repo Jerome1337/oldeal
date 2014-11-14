ca940ceec97cf6c887b6a6107822309c791336c6
Jerome1337 <jeromepogeant@MBP-de-Jerome.wifi.froment.in.iesanetwork.com>
2014-11-14 09:26:17 +0100

    no message

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
 			<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 		</head>
 		<body>
-			<!--<p id="bienvenue" data-18n="Bienvenue" data-i18n-en="Welcome" data-i18n-de="Wilkommen"><p>-->
-			<p id="hello" data-18n="Hello" data-i18n-de="Hallo" data-i18n-ru="Привет"><p>
+			
 		</body>
 	</html>
