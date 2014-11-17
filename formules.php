f9b17f4e15589245513d50191ccaab018310ebda
Jerome1337 <jeromepogeant@pc18.home>
2014-11-17 20:41:02 +0100

    add page formulas + modify liens include nav et add css formules

---
+++ formules.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<?php include('include/head.php'); ?>
+			<title>Oldeal</title>
+		</head>
+		<body>
+			<header>
+				<?php include('include/nav-top.php'); ?>			
+			</header>
+			<section id="formules">
+				<h1>Les forfaits</h1>
+				<p>Afin de vous simplifier la vie, nous avons conçu des forfaits adaptés à vos besoins :</p>
+				<p>Pour plus d'information n'hésitez pas à <a href="contact.php">nous contacter</a></p>
+			</section>
+			<footer>
+				<?php include('include/nav-bottom.php'); ?>
+			</footer>
+			<script src="js/vendor/jquery.js"></script>
+			<script src="js/vendor/fastclick.js"></script>
+			<script src="js/foundation/foundation.js"></script>
+			<script src="js/foundation/foundation.orbit.js"></script>
+			<script>
+			  $(document).foundation();
+			</script>
+		</body>
+	</html>
